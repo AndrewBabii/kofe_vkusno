57b589b24180bb08d07f4262dfae00bba0e1bef6
Ruslan Orolbaev <ruslan.orolbaev@gmail.com>
2022-02-02 19:24:38 +0600

    conflicts resolved

--- lib/ui/shared/main_layout.dart
+++ lib/ui/shared/main_layout.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:kofe_vkusno/ui/shared/widgets/map_screen.dart';
 import 'package:kofe_vkusno/ui/shared/widgets/app_bar_custom/app_bar_custom.dart';
 import 'package:kofe_vkusno/ui/shared/widgets/coffee_drawer/coffee_drawer.dart';
-
 
 class MainLayout extends StatelessWidget {
   const MainLayout({Key? key}) : super(key: key);
--- lib/ui/shared/widgets/coffee_drawer/coffee_drawer.dart
+++ lib/ui/shared/widgets/coffee_drawer/coffee_drawer.dart
@@ -3,20 +3,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kofe_vkusno/providers.dart';
 import 'package:kofe_vkusno/ui/shared/widgets/coffee_drawer/button_drawer.dart';
 
-
 final coffeeDrawer = Drawer(
     child: Column(children: const [
-        SizedBox(height: 120),
-        ButtonDrawer(textButton: 'Отсканировать код', routName: '/qr-scanner'),
-        ButtonDrawer(textButton: 'Бесплатная чашка', routName: '/cup'),
-        TestZeroCupButtonDrawer()
-      ]
-    )
-);
+  SizedBox(height: 120),
+  ButtonDrawer(label: 'Отсканировать код', routName: '/qr-scanner'),
+  ButtonDrawer(label: 'Бесплатная чашка', routName: '/cup'),
+  ButtonDrawer(label: 'Map Screen', routName: '/mapScreen'),
+  TestZeroCupButtonDrawer()
+]));
 
-
-class TestZeroCupButtonDrawer extends ConsumerWidget{
-
+class TestZeroCupButtonDrawer extends ConsumerWidget {
   const TestZeroCupButtonDrawer({Key? key}) : super(key: key);
 
   @override
@@ -27,19 +23,17 @@ class TestZeroCupButtonDrawer extends ConsumerWidget{
           Navigator.of(context).pop();
         },
         style: ButtonStyle(
-            fixedSize: MaterialStateProperty.all<Size>(const Size.fromWidth(double.maxFinite)),
-            backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF1F1005)),
-
+          fixedSize: MaterialStateProperty.all<Size>(
+              const Size.fromWidth(double.maxFinite)),
+          backgroundColor:
+              MaterialStateProperty.all<Color>(const Color(0xFF1F1005)),
         ),
         child: const Text(
           'Обнулить колличество чашек',
           style: TextStyle(
               fontSize: 18,
               color: Color(0xFFFFF7e5),
-              fontStyle: FontStyle.italic
-          ),
-        )
-    );
+              fontStyle: FontStyle.italic),
+        ));
   }
-
 }
