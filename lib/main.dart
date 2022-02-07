59964bc13de71152c8b821bbdcee0514fe6a96b9
Ruslan Orolbaev <ruslan.orolbaev@gmail.com>
2022-02-07 14:41:59 +0600

    map_box_settings.dart introduced constants from map_screen.dart has been moved to map_box_settings.dart and urls.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:kofe_vkusno/ui/shared/beverage_list_screen.dart';
 import 'package:kofe_vkusno/ui/shared/main_layout.dart';
 import 'package:kofe_vkusno/ui/shared/qr_screen.dart';
 import 'package:kofe_vkusno/ui/shared/cup_screen.dart';
-import 'package:kofe_vkusno/ui/shared/widgets/map_screen.dart';
+import 'package:kofe_vkusno/ui/shared/map_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
